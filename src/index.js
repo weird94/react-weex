f8796b5b05eaae236652d0cc0c8a344e77679edf
zhangw <935279748@qq.com>
2019-11-19 13:17:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 
 import React from 'react';
 import ReactWeex from './react-weex';
+import alert from 'universal-alert';
 
 import App from './App';
 
 ReactWeex.render(<App />);
 
-// alert('success');
+alert('success');
